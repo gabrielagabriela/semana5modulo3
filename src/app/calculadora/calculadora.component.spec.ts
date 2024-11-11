2a5539bb54dc217e31781aa7d77f076e575ab371
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-11-11 13:06:12 -0300

    ex 07

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 import { CalculadoraService } from '../service/calculadora.service';
+import { FormsModule } from '@angular/forms';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -13,6 +14,7 @@ describe('CalculadoraComponent', () => {
     
     TestBed.configureTestingModule({
       declarations: [CalculadoraComponent],
+      imports: [FormsModule],  
       providers: [
         { provide: CalculadoraService, useValue: calculadoraServiceSpy }
       ]
@@ -23,12 +25,22 @@ describe('CalculadoraComponent', () => {
     calculadoraService = TestBed.inject(CalculadoraService) as jasmine.SpyObj<CalculadoraService>;
   });
 
-  it('deve atualizar o resultado corretamente ao somar dois números', () => {
+  it('deve atualizar as variáveis num1 e num2 ao alterar os campos de entrada', () => {
+    const inputNum1 = fixture.nativeElement.querySelector('input[placeholder="Número 1"]');
+    const inputNum2 = fixture.nativeElement.querySelector('input[placeholder="Número 2"]');
+    inputNum1.value = 10;
+    inputNum1.dispatchEvent(new Event('input'));
+    inputNum2.value = 5;
+    inputNum2.dispatchEvent(new Event('input'));
+    expect(component.num1).toBe(10);
+    expect(component.num2).toBe(5);
+  });
 
+  it('deve atualizar o resultado corretamente ao somar dois números', () => {
     calculadoraService.somar.and.returnValue(15);
     component.num1 = 10;
     component.num2 = 5;
-    component.somarValores();
+    component.somarValores(); 
     expect(component.result).toBe(15);
     expect(calculadoraService.somar).toHaveBeenCalledWith(10, 5);
   });
