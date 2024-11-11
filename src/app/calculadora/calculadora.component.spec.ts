052f76a757586247beebadf0e87674c5be4c7fe6
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-11-11 13:03:05 -0300

    ex 06

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../service/calculadora.service';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
   let fixture: ComponentFixture<CalculadoraComponent>;
+  let calculadoraService: jasmine.SpyObj<CalculadoraService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CalculadoraComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    const calculadoraServiceSpy = jasmine.createSpyObj('CalculadoraService', ['somar']);
+    
+    TestBed.configureTestingModule({
+      declarations: [CalculadoraComponent],
+      providers: [
+        { provide: CalculadoraService, useValue: calculadoraServiceSpy }
+      ]
+    });
 
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    calculadoraService = TestBed.inject(CalculadoraService) as jasmine.SpyObj<CalculadoraService>;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve atualizar o resultado corretamente ao somar dois números', () => {
+
+    calculadoraService.somar.and.returnValue(15);
+    component.num1 = 10;
+    component.num2 = 5;
+    component.somarValores();
+    expect(component.result).toBe(15);
+    expect(calculadoraService.somar).toHaveBeenCalledWith(10, 5);
   });
+
 });
